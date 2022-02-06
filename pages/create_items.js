8a876bc69c5355258736aeb0d3ef7f7a95bb0054
aayusshhh <bhardwajaayush0202@gmai.com>
2022-02-07 00:09:08 +0530

    s

--- pages/create_items.js
+++ pages/create_items.js
@@ -162,10 +162,116 @@ const create_items = () => {
                 </option>
                 <option value="collect" >PETI(PET)</option>
               </select>
+
             </div>
 
             <div className={CI.Aunit}>
-              
+              <span>Enable Alternative Units</span>
+
+            </div>
+
+          </div>
+
+          <div className={CI.Sunit}>
+            <div className={CI.SU}>
+              <span>Secondary Unit</span>
+              <select>
+                <option value="" disabled selected>Enter Unit </option>
+                <option value="collect" >PETI(PET)</option>
+              </select>
+
+            </div>
+
+            <div className={CI.CR}>
+              <span>Conversion Rate</span>
+              <input type="number" placeholder="Enter Conversion Rate(1PET=)"></input>
+            </div>
+          </div>
+
+
+          <div className={CI.OS}>
+            <div className={CI.Ostock}>
+              <span>Opening Stock</span>
+              <input type="number" placeholder="0 PET"></input>
+
+            </div>
+
+            <div className={CI.ADate}>
+              <span>As Of Date</span>
+              <input type="date" placeholder="Enter Conversion Rate(1PET=)"></input>
+            </div>
+          </div>
+
+          <div className={CI.LS}>
+
+            <div className={CI.Lunit}>
+              <span>Enable Low Stock Warning</span>
+
+            </div>
+
+            <div className={CI.LSU}>
+              <span>Low Stock Unit</span>
+              <input type="number" placeholder="Enter Low Stock Unit In PET"></input>
+
+            </div>
+
+
+          </div>
+
+          <div className={CI.PD}>
+            <span>Pricing Details</span>
+          </div>
+
+          <div className={CI.SP}>
+
+            <div className={CI.Sbalance}>
+              Sales Price
+              <div className={CI.Sbal}>
+
+
+                <input placeholder='₹0' name="openingBalance" />
+                <select >
+                  <option value="collect">With Tax</option>
+                  <option value="pay">Without Tax</option>
+                </select>
+              </div>
+            </div>
+
+            <div className={CI.Pbalance}>
+              Purchase Price
+              <div className={CI.Pbal}>
+
+
+                <input placeholder='₹0' name="openingBalance" />
+                <select >
+                  <option value="collect">With Tax</option>
+                  <option value="pay">Without Tax</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className={CI.tax}>
+            <div className={CI.HC}>
+              <span>HSN Code</span>
+              <input type="number" placeholder="Enter HSN Code"></input>
+            </div>
+            <div className={CI.Gst}>
+
+              <span>GST Tax Rate(%)</span>
+              <select>
+                <option value="" disabled selected>GST@0% </option>
+                <option value="collect" >GST@0.1%</option>
+                <option value="collect" >GST@0.25%</option>
+                <option value="collect" >GST@3%</option>
+                <option value="collect" >GST@5%</option>
+                <option value="collect" >GST@12%</option>
+                <option value="collect" >GST@18%</option>
+                <option value="collect" >GST @ 14% + cess @ 12%</option>
+                <option value="collect" >GST@28%</option>
+                <option value="collect" >GST @ 28% + cess @ 12%</option>
+              </select>
+
             </div>
 
           </div>
